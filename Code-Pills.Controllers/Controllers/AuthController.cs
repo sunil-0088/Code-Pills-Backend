c3ed069b5488d18290bd974a0234d4c9259a56be
Gopal <gopal.sharma@qburst.com>
2024-02-15 16:35:16 +0530

    Added token and google sign in action method

---
+++ Code-Pills.Controllers/Controllers/AuthController.cs
@@ -0,0 +1,155 @@
+﻿using Code_Pills.Services.DTOs;
+using Code_Pills.Services.Interface;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Code_Pills.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IJwtToken tokenService;
+
+        public AuthController( UserManager<IdentityUser> userManager, IJwtToken tokenService)
+        {
+            this.userManager = userManager;
+            this.tokenService = tokenService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            // Create Identity user
+            var user = new IdentityUser
+            {
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim(),
+            };
+            var identityResult = await userManager.CreateAsync(user, request.Password);
+            if(identityResult.Succeeded)
+            {
+                identityResult = await userManager.AddToRoleAsync(user, "User");
+                if(identityResult.Succeeded)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    if(identityResult.Errors.Any())
+                    {
+                        foreach(var error in identityResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (identityResult.Errors.Any())
+                {
+                    foreach(var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return ValidationProblem(ModelState);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            // checking email
+            var identityUser = await userManager.FindByEmailAsync(request.Email);
+
+            if(identityUser != null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(identityUser,request.Password);
+                if(checkPasswordResult)
+                {
+                    var roles = await userManager.GetRolesAsync(identityUser);
+
+                    // Create a Token and response
+                    var token = tokenService.CreateToken(identityUser, roles.ToList());
+                    var response = new LoginResponseDto()
+                    {
+                        Email = request.Email,
+                        Roles = roles.ToList(),
+                        Token = token
+
+                    };
+                    return Ok(response);
+                }
+            }
+            ModelState.AddModelError("", "Email or password is incorrect");
+            return Ok();
+        }
+
+        // Handling Google signing
+
+        [HttpPost("google")]
+        public async Task<IActionResult> GoogleSignin([FromBody] GoogleRequestDto request)
+        {
+            // checking email
+            var identityUser = await userManager.FindByEmailAsync(request.Email);
+            //Sign in
+            if (identityUser != null)
+            {
+                var roles = await userManager.GetRolesAsync(identityUser);
+
+                // Create a Token and response
+                var token = tokenService.CreateToken(identityUser, roles.ToList());
+                var response = new LoginResponseDto()
+                {
+                    Email = request.Email,
+                    Roles = roles.ToList(),
+                    Token = token
+
+                };
+                return Ok(response);
+            }
+            // SignUp
+            else
+            {
+                var user = new IdentityUser
+                {
+                    UserName = request.Email?.Trim(),
+                    Email = request.Email?.Trim(),
+                };
+                var identityResult = await userManager.CreateAsync(user);
+                if (identityResult.Succeeded)
+                {
+                    identityResult = await userManager.AddToRoleAsync(user, "User");
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        if (identityResult.Errors.Any())
+                        {
+                            foreach (var error in identityResult.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (identityResult.Errors.Any())
+                    {
+                        foreach (var error in identityResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+                return ValidationProblem(ModelState);
+            }
+        }
+    }
+}
---
+++ Code-Pills.DataAccess/Context/AuthDbContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Pills.DataAccess.Context
+{
+    public class AuthDbContext:IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options): base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var userRoleId = "82e28843-2239-4acd-9e85-0c71bf41e466";
+            var contributorRoleId = "1dfb1641-47c7-47a6-b4f8-1ca9b92fbf38";
+
+            // Creating Roles
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id=userRoleId,
+                    Name="User",
+                    NormalizedName="User".ToUpper(),
+                    ConcurrencyStamp= userRoleId
+                },
+                new IdentityRole()
+                {
+                    Id = contributorRoleId,
+                    Name = "Contributor",
+                    NormalizedName = "Contributor".ToUpper(),
+                    ConcurrencyStamp = contributorRoleId
+                },
+
+            };
+            // seed roles
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            //Create an admin user
+            var adminUserId = "0d88f3e7-d8ad-40eb-8c9b-85e45e83bdb0";
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "admin@codepills.com",
+                Email = "admin@codepills.com",
+                NormalizedEmail = "admin@codepills.com".ToUpper(),
+                NormalizedUserName = "admin@codepills.com".ToUpper()
+            };
+            admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin@123");
+            builder.Entity<IdentityUser>().HasData(admin);
+
+            // Give roles to admin
+
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = userRoleId
+                },
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = contributorRoleId
+                }
+            };
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+        }
+    }
+}
